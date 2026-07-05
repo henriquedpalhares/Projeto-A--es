f843f9d567bdffb758ae2b484db9190279ac96d8
Henrique Davi <henrique.d.palhares@gmail.com>
2026-07-05 11:52:22 -0300

    teste ações

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Teste")
+import requests
+import json
+
+acao = input("Qual ação você quer? ").upper()
+
+url = f"https://brapi.dev/api/v2/stocks/quote?symbols={acao}"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    dados = response.json()
+
+    print("Status code:", response.status_code)
+    print(json.dumps(dados, indent=4, ensure_ascii=False))
+
+else:
+    print("Erro:", response.status_code)
